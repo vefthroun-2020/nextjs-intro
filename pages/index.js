32f35ac95cd387ced28b6776c83b4ff59c362d6b
brisa-pedronetto <pedro@brisa.is>
2021-02-08 14:43:40 +0000

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,48 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
 
-export default function Home() {
+export default function Home({ people }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Homepage</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <ul className={styles.peopleList}>
+        {people.map((person) => (
+          <li>
+            <div>
+              <img src={person.avatar} />
+            </div>
+            <div>
+              {person.first_name} {person.last_name}
+            </div>
+            <div>{person.country}</div>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps(context) {
+  const res = await fetch(`https://my.api.mockaroo.com/people.json`, {
+    headers: {
+      "X-API-Key": "632d7f80",
+    },
+  });
+  const dummyPeople = await res.json();
+
+  if (!dummyPeople) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      people: dummyPeople,
+    },
+  };
 }
